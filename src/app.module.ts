0acf591fc6403441961e3abc52a13c98c5fa4bad
irfandfr <irfandfr@gmail.com>
2024-09-18 01:21:14 +0700

    add task entity add configmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,21 +6,24 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { UserResolver } from './graphql/resolvers/UserResolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './graphql/models/User';
+import { Task } from './graphql/models/Task';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'src/schema.gql'
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'root',
-      database: 'nestapi',
-      entities: [User],
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [User, Task],
       synchronize: true,
     }),
   ],
---
+++ src/graphql/models/Task.ts
@@ -0,0 +1,33 @@
+import { Field, Int, ObjectType } from "@nestjs/graphql";
+import { Column, CreateDateColumn, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity({name: 'tasks'})
+@ObjectType()
+export class Task{
+  @PrimaryGeneratedColumn()
+  @Field((type) => Int)
+  id: number;
+
+  @Column('text')
+  @Field()
+  title: string;
+
+  @Column('text')
+  @Field()
+  description: string;
+
+  @Column({type: 'text', default: "open"})
+  @Field()
+  status: 'open' | 'in-progress' | 'done';
+
+  @OneToOne(() => Task)
+  @JoinColumn()
+  @Field(() => Task,{nullable:true})
+  taskDependency? : Task
+
+  @CreateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP(6)" })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP(6)", onUpdate: "CURRENT_TIMESTAMP(6)" })
+  updatedAt: Date;
+}
