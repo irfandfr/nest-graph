de678c5aa6714f5005c30d6ad42fc5d721127e68
irfandfr <irfandfr@gmail.com>
2024-09-18 01:00:15 +0700

    add user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,25 @@ import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { UserResolver } from './graphql/resolvers/UserResolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './graphql/models/User';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'src/schema.gql'
-    })
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'nestapi',
+      entities: [User],
+      synchronize: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService, UserResolver],
--- src/graphql/models/User.ts
+++ src/graphql/models/User.ts
@@ -1,14 +1,19 @@
 import { Field, Int, ObjectType } from "@nestjs/graphql";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+@Entity({ name: 'users' })
 @ObjectType()
 export class User{
 
+  @PrimaryGeneratedColumn()
   @Field((type) => Int)
   id: number;
 
+  @Column()
   @Field()
   email: string;
 
+  @Column()
   @Field({nullable: true})
   role: string
 }
