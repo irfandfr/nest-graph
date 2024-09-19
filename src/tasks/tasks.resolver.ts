325d0362d6fb550067ec9c68999bfa2c6e0bd427
irfandfr <irfandfr@gmail.com>
2024-09-19 21:18:55 +0700

    add public decorator to resolver

--- src/tasks/tasks.resolver.ts
+++ src/tasks/tasks.resolver.ts
@@ -3,26 +3,31 @@ import { TasksService } from './tasks.service';
 import { Task } from './entities/task.entity';
 import { CreateTaskInput } from './dto/create-task.input';
 import { UpdateTaskInput } from './dto/update-task.input';
+import { Public } from 'src/decorators/public.decorator';
 
 @Resolver(() => Task)
 export class TasksResolver {
   constructor(private readonly tasksService: TasksService) {}
 
+  
   @Mutation(() => Task)
   createTask(@Args('createTaskInput') createTaskInput: CreateTaskInput) {
     return this.tasksService.create(createTaskInput);
   }
 
+  @Public()
   @Query(() => [Task], { name: 'tasks' })
   findAll() {
     return this.tasksService.findAll();
   }
   
+  @Public()
   @Query(() => [Task], { name: 'readyTasks' })
   findReady() {
     return this.tasksService.findReady();
   }
 
+  @Public()
   @Query(() => Task, { name: 'task' })
   findOne(@Args('id', { type: () => Int }) id: number) {
     return this.tasksService.findOne(id);
