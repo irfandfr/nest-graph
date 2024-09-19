4a3223b3899fc5d946fd1e0cd9ec135fece6940e
irfandfr <irfandfr@gmail.com>
2024-09-19 21:17:13 +0700

    add global guard on task module

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -3,9 +3,14 @@ import { TasksService } from './tasks.service';
 import { TasksResolver } from './tasks.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './entities/task.entity';
+import { APP_GUARD } from '@nestjs/core';
+import { GqlAuthGuard } from 'src/auth/gql-auth.guard';
 
 @Module({
   imports:[TypeOrmModule.forFeature([Task])],
-  providers: [TasksResolver, TasksService],
+  providers: [TasksResolver, TasksService, {
+    provide: APP_GUARD,
+    useClass: GqlAuthGuard
+  }],
 })
 export class TasksModule {}
