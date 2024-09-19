3357b3c32886c5a8b6e4a55baf57b7a89cf9986c
irfandfr <irfandfr@gmail.com>
2024-09-20 02:40:11 +0700

    add remove dependency

--- src/tasks/tasks.resolver.ts
+++ src/tasks/tasks.resolver.ts
@@ -5,6 +5,7 @@ import { CreateTaskInput } from './dto/create-task.input';
 import { UpdateTaskInput } from './dto/update-task.input';
 import { Public } from 'src/decorators/public.decorator';
 import { Roles } from 'src/decorators/role.decorator';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
 @Resolver(() => Task)
 export class TasksResolver {
@@ -31,9 +32,17 @@ export class TasksResolver {
   @Public()
   @Query(() => Task, { name: 'task' })
   findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.tasksService.findOne(id);
+    const task = this.tasksService.findOne(id);
+    if(!task)
+    throw new HttpException('Task not found', HttpStatus.BAD_REQUEST);
+    return task
   }
   
+  @Mutation(() => String)
+  removeDependency(@Args('id', {type: () => Int}) id:number){
+    const res = this.tasksService.removeDependency(id)
+    return res ? 'Dependency remove' : 'Dependency not removed'
+  }
 
   @Mutation(() => Task, {nullable: true})
   updateTask(@Args('updateTaskInput') updateTaskInput: UpdateTaskInput) {
