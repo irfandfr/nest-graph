390f9274710b92af4d5084d0bf39620a69c4bf73
irfandfr <irfandfr@gmail.com>
2024-09-19 21:17:59 +0700

    add exception on guard

--- src/auth/gql-auth.guard.ts
+++ src/auth/gql-auth.guard.ts
@@ -1,11 +1,26 @@
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { GqlExecutionContext } from '@nestjs/graphql';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from 'src/decorators/public.decorator';
 
 @Injectable()
 export class GqlAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector){
+    super()
+  }
+
   getRequest(context: ExecutionContext) {
     const ctx = GqlExecutionContext.create(context);
     return ctx.getContext().req;
   }
+
+  canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride(IS_PUBLIC_KEY, [
+           context.getHandler(),
+           context.getClass(),
+    ]);
+    return isPublic ? true : super.canActivate(context)
+ }
+ 
 }
